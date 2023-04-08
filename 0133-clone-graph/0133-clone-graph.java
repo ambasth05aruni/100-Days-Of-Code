fca57aca10c563a22c5912d4d11b812fb0fe1f1f
Arunima Ambastha <101694582+ambasth05aruni@users.noreply.github.com>
2023-04-08 21:50:45 +0530

    Time: 24 ms (99.84%), Space: 42.5 MB (73.52%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+                       return cloneDFS(node, new HashMap<>());
+    }
+    private Node cloneDFS(Node node, Map<Node, Node> map){
+        if(node == null) return null;
+        if(map.containsKey(node)) return map.get(node);
+        
+        Node newNode = new Node(node.val);
+		//preorder part
+        map.put(node, newNode);
+        
+        for(Node nei: node.neighbors){
+            newNode.neighbors.add(cloneDFS(nei, map));
+        }
+        
+        return newNode;
+    }
+}
