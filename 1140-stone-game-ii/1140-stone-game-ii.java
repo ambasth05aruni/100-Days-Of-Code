0058abf0f9754d7eb9aef9825bfa5a377805fdf9
Arunima Ambastha <101694582+ambasth05aruni@users.noreply.github.com>
2023-05-26 21:12:21 +0530

    Time: 13 ms (35.59%), Space: 41.2 MB (100.00%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+      final int n = piles.length;
+for (int i = n - 2; i >= 0; i--) piles[i] += piles[i+1];
+if (n <= 2) return piles[0];
+int[][] dp = new int[n][(n+1)/2+1];
+for (int i = n-1; i >= 0; i--) {
+    int sum = piles[i];
+    int m = (n-i+1)/2;
+    dp[i][m] = sum;
+    while (--m > 0) {
+        dp[i][m] = 0;
+        for (int x = 1; x <= m * 2 && i+x < n; x++) {
+            int mx = Math.min((n-i-x+1)/2, Math.max(x, m));
+            dp[i][m] = Math.max(dp[i][m], sum - dp[i+x][mx]);
+        }
+    }
+}
+return dp[0][1];  
+    }
+}
